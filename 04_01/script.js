1c4377cc59e8f29ed37cccd01c2ef6746e444efc
fanweikung <fan-wei.kung@unicodeconsulting.com>
2022-03-17 03:32:28 -0700

    The old way of using string to render html content

--- 04_01/script.js
+++ 04_01/script.js
@@ -16,24 +16,7 @@ const everydayPack = new Backpack(
   "December 5, 2018 15:00:00 PST"
 );
 
-const content = `
-<main>
-<article>
-  <h1>${everydayPack.name}</h1>
-  <ul>
-    <li>Volume: ${everydayPack.volume}</li>
-    <li>Color: ${everydayPack.color}</li>
-    <li>Age: ${everydayPack.backpackAge()}</li>
-    <li>Number of pockets: ${everydayPack.pocketNum}</li>
-    <li>Left strap length: ${everydayPack.strapLength.left}</li>
-    <li>Right strap length: ${everydayPack.strapLength.right}</li>
-    <li>Lid status: ${everydayPack.lidOpen}</li>
-  </ul>
-</article>
-</main>
-`;
-
-document.body.innerHTML = content;
+document.body.innerHTML = "<h1> Name goes here: " + everydayPack.name + "</h1>";
 
 console.log("The everydayPack object:", everydayPack);
 console.log("The pocketNum value:", everydayPack.pocketNum);
