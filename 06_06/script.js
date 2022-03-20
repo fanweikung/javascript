24d5a18730c8629af75367254082d19bdf1ca3a4
fanweikung <fan-wei.kung@unicodeconsulting.com>
2022-03-20 16:27:30 -0700

    Type of values

--- 06_06/script.js
+++ 06_06/script.js
@@ -5,11 +5,11 @@
 
 // String:
 let stringDemo = "A string of text.";
-console.log("String:", stringDemo);
+console.log("String:", typeof stringDemo);
 
 // Numbes:
 let integerDemo = 4;
-console.log("Integer:", integerDemo);
+console.log("Integer:", typeof integerDemo);
 
 let floatDemo = 5.6;
 console.log("Floating point number:", floatDemo);
