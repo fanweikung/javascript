6f26a15ec56b84fdc77c3cf7edde4697e8ee9c8d
fanweikung <fan-wei.kung@unicodeconsulting.com>
2022-03-22 12:01:02 -0700

    Advanced function - array of backpacks

--- 08_16/script.js
+++ 08_16/script.js
@@ -1,59 +1,60 @@
 /**
- * Challenge: Create an advanced function.
+ * Solution: Create an advanced function.
  * - Loop through backpackObjectArray to create an article with the class "backpack".
  * - Give the article the ID of the current backpack object.
  * - Set the inner HTML of the article to the existing HTML output provided in const content.
  * - Append each backpack object to the <main> element.
  */
-import Backpack from "./components/Backpack.js";
+import backpackObjectArray from "./components/data.js";
 
-const everydayPack = new Backpack(
-  "pack01",
-  "Everyday Backpack",
-  30,
-  "grey",
-  15,
-  26,
-  26,
-  false,
-  "December 5, 2018 15:00:00 PST",
-  "../assets/images/everyday.svg"
-);
+// Map throught the array and make a new array
+const content = backpackObjectArray.map((backpack) => {
+  // "backpack" now holds a single backpack object
 
-const content = `
+  // Create new article
+  let backpackArticle = document.createElement("article");
+  backpackArticle.classList.add("backpack");
+
+  // Set article ID to the backpack.id property
+  backpackArticle.setAttribute("id", backpack.id);
+
+  // Set the innerHTML of backpackArticle using the backpack object.
+  backpackArticle.innerHTML = `
     <figure class="backpack__image">
-      <img src=${everydayPack.image} alt="" />
+      <img src=${backpack.image} alt="" loading="lazy" />
     </figure>
-    <h1 class="backpack__name">${everydayPack.name}</h1>
+    <h1 class="backpack__name">${backpack.name}</h1>
     <ul class="backpack__features">
-      <li class="packprop backpack__volume">Volume:<span> ${
-        everydayPack.volume
+      <li class="feature backpack__volume">Volume:<span> ${
+        backpack.volume
       }l</span></li>
-      <li class="packprop backpack__color">Color:<span> ${
-        everydayPack.color
+      <li class="feature backpack__color">Color:<span> ${
+        backpack.color
       }</span></li>
-      <li class="backpack__age">Age:<span> ${everydayPack.backpackAge()} days old</span></li>
-      <li class="packprop backpack__pockets">Number of pockets:<span> ${
-        everydayPack.pocketNum
+      <li class="feature backpack__age">Age:<span> ${backpack.backpackAge()} days old</span></li>
+      <li class="feature backpack__pockets">Number of pockets:<span> ${
+        backpack.pocketNum
       }</span></li>
-      <li class="packprop backpack__strap">Left strap length:<span> ${
-        everydayPack.strapLength.left
+      <li class="feature backpack__strap">Left strap length:<span> ${
+        backpack.strapLength.left
       } inches</span></li>
-      <li class="packprop backpack__strap">Right strap length:<span> ${
-        everydayPack.strapLength.right
+      <li class="feature backpack__strap">Right strap length:<span> ${
+        backpack.strapLength.right
       } inches</span></li>
       <li class="feature backpack__lid">Lid status:<span> ${
-        everydayPack.lidOpen ? "open" : "closed"
+        backpack.lidOpen ? "open" : "closed"
       }</span></li>
     </ul>
-  
-`;
+  `;
 
+  // Return the backpackArticle to the content array.
+  return backpackArticle;
+});
+
+// Get the main
 const main = document.querySelector(".maincontent");
 
-const newArticle = document.createElement("article");
-newArticle.classList.add("backpack");
-newArticle.setAttribute("id", "everyday");
-newArticle.innerHTML = content;
-
-main.append(newArticle);
+// Loop through the content array to append each backpack article.
+content.forEach((backpack) => {
+  main.append(backpack);
+});
