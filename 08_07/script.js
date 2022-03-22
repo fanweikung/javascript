8a705027116ef576df256034eefca26e99f77a74
fanweikung <fan-wei.kung@unicodeconsulting.com>
2022-03-22 02:20:37 -0700

    Tip Calculator - reset for formatter

--- 08_07/script.js
+++ 08_07/script.js
@@ -1,26 +1,26 @@
 /**
  * Passing data to functions through parameters.
  * @link https://developer.mozilla.org/en-US/docs/Glossary/Function
+ * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat
+ *
+ *
+ * List of ISO language codes:
+ * @link http://www.lingoes.net/en/translator/langcode.htm
  */
 
-const tipCalculator = (sum, percentage, currency, prefix) => {
-  let tip = sum * (percentage / 100);
-  let total = sum + tip;
-  if (prefix === true) {
-    console.log(`
-  Sum before tip: ${currency}${sum}
-  Tip percentage: ${percentage}%
-  Tip:            ${tip.toFixed(2)}
-  Total:          ${currency}${total.toFixed(2)}
-`);
-  } else {
-    console.log(`
-    Sum before tip: ${sum}${currency}
-    Tip percentage: ${percentage}%
-    Tip:            ${tip.toFixed(2)}
-    Total:          ${total.toFixed(2)}${currency}
-  `);
-  }
+const formatter = (symbol) => {
+  return symbol;
 };
 
-tipCalculator(29.95, 18, "$", false);
+const tipCalculator = (sum, percentage, currency) => {
+  let tip = sum * (percentage / 100);
+  let total = sum + tip;
+  console.log(`
+      Sum before tip: ${formatter(currency)}${sum}
+      Tip percentage: ${percentage}%
+      Tip:            ${tip.toFixed(2)}
+      Total:          ${formatter(currency)}${total.toFixed(2)}
+    `);
+};
+
+tipCalculator(29.95, 18, "USD");
